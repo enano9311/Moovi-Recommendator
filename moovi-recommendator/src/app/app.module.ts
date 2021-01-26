827b5ee6d2c793e6342a9d69dc50091985957262
Marc Vredeveld <marcv@wolfram.com>
2021-01-25 23:06:36 -0600

    Add call to tmdb movie API. Successfully logged Observable. TODO: parse JSON data

--- moovi-recommendator/src/app/app.module.ts
+++ moovi-recommendator/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule
   ],
--- moovi-recommendator/src/app/my-component/my-component.component.ts
+++ moovi-recommendator/src/app/my-component/my-component.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {Movie} from '../movie'
-import {FAKEMOVIES} from '../fake-movies'
+import { Component, OnInit, Injectable } from '@angular/core';
+import {MovieService} from '../services/movie.service';
+import {FAKEMOVIES} from '../fake-movies';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
   styleUrls: ['./my-component.component.css']
 })
+
+@Injectable()
 export class MyComponentComponent implements OnInit {
 
+  constructor(private client: HttpClient){}
   fakies = FAKEMOVIES;
-
-  constructor() { }
   
   ngOnInit(): void {
+    let service = new MovieService(this.client);
+    console.log(service.getMovie('123'));
   }
 
 }
